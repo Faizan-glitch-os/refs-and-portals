b8aec8f6b519cd77577a4e1fe36b16b46d82bddb
Faizan Ahmad <faizanahmad2582001@gmail.com>
2025-04-15 18:42:23 +0500

    modal can be shown automatically after time expires and can be shown when button is clicked, but results to show in the modal still remains

--- src/components/timer-challenge.jsx
+++ src/components/timer-challenge.jsx
@@ -3,36 +3,43 @@ import ResultModal from "./result-modal";
 
 export default function TimerChallenge({ title, targetTime }) {
   const timer = useRef();
+  const modal = useRef();
 
-  const [timeExpired, setTimeExpired] = useState(false);
-  const [timeStarted, setTimeStarted] = useState(false);
+  const [remainingTime, setRemainingTime] = useState(targetTime * 1000);
+
+  const isTimeActive = remainingTime >= 0 && remainingTime < targetTime * 1000;
+
+  if (remainingTime <= 0) {
+    clearInterval(timer.current);
+    setRemainingTime(targetTime * 1000);
+    modal.current.open();
+  }
 
   function handleStart() {
-    timer.current = setTimeout(() => {
-      setTimeExpired(true);
-    }, targetTime * 1000);
-
-    setTimeStarted(true);
+    timer.current = setInterval(() => {
+      setRemainingTime((previousTime) => previousTime - 10);
+    }, 10);
   }
 
   function handleStop() {
-    clearTimeout(timer.current);
-    setTimeStarted(false);
+    modal.current.open();
+    clearInterval(timer.current);
   }
 
   return (
     <>
-      {timeExpired && <ResultModal result="lost" targetTime={targetTime} />}
+      <ResultModal result="lost" targetTime={targetTime} ref={modal} />
+
       <section className="challenge">
         <h2>{title}</h2>
         <p className="challeng-time">{targetTime} second(s)</p>
         <p>
-          <button onClick={timeStarted ? handleStop : handleStart}>
+          <button onClick={isTimeActive ? handleStop : handleStart}>
             {timeStarted ? "Stop" : "Start"} Challenge
           </button>
         </p>
-        <p className={timeStarted ? "active" : undefined}>
-          {timeStarted ? "Time is running...." : "Time is stopped"}
+        <p className={isTimeActive ? "active" : undefined}>
+          {isTimeActive ? "Time is running...." : "Time is stopped"}
         </p>
       </section>
     </>
