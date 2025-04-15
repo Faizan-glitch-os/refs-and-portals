25c7dced6123dc030c800e951d39cdbcae37c7b3
Faizan Ahmad <faizanahmad2582001@gmail.com>
2025-04-15 16:11:59 +0500

    dialog box is desinged and opens when time expires, but full functionality still remains

--- src/components/timer-challenge.jsx
+++ src/components/timer-challenge.jsx
@@ -1,4 +1,5 @@
 import { useRef, useState } from "react";
+import ResultModal from "./result-modal";
 
 export default function TimerChallenge({ title, targetTime }) {
   const timer = useRef();
@@ -20,17 +21,20 @@ export default function TimerChallenge({ title, targetTime }) {
   }
 
   return (
-    <section className="challenge">
-      <h2>{title}</h2>
-      <p className="challeng-time">{targetTime} second(s)</p>
-      <p>
-        <button onClick={timeStarted ? handleStop : handleStart}>
-          {timeStarted ? "Stop" : "Start"} Challenge
-        </button>
-      </p>
-      <p className={timeStarted ? "active" : undefined}>
-        {timeStarted ? "Time is running...." : "Time is stopped"}
-      </p>
-    </section>
+    <>
+      {timeExpired && <ResultModal result="lost" targetTime={targetTime} />}
+      <section className="challenge">
+        <h2>{title}</h2>
+        <p className="challeng-time">{targetTime} second(s)</p>
+        <p>
+          <button onClick={timeStarted ? handleStop : handleStart}>
+            {timeStarted ? "Stop" : "Start"} Challenge
+          </button>
+        </p>
+        <p className={timeStarted ? "active" : undefined}>
+          {timeStarted ? "Time is running...." : "Time is stopped"}
+        </p>
+      </section>
+    </>
   );
 }
